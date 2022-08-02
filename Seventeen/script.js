48988788032d275811cb3bbcbda245a9cbf626df
AbindraSureshKumar <86580143+AbindraSureshKumar@users.noreply.github.com>
2022-08-02 16:52:50 +0530

    Add files via upload

---
+++ Seventeen/script.js
@@ -0,0 +1,31 @@
+let countDown=10;
+setTimeout(()=>{
+    document.querySelector(".countdown").innerText =--countDown;
+    setTimeout(()=>{
+        document.querySelector(".countdown").innerText =--countDown;
+        setTimeout(()=>{
+            document.querySelector(".countdown").innerText =--countDown;
+            setTimeout(()=>{
+                document.querySelector(".countdown").innerText =--countDown;
+                setTimeout(()=>{
+                    document.querySelector(".countdown").innerText =--countDown;
+                    setTimeout(()=>{
+                        document.querySelector(".countdown").innerText =--countDown;
+                        setTimeout(()=>{
+                            document.querySelector(".countdown").innerText =--countDown;
+                            setTimeout(()=>{
+                                document.querySelector(".countdown").innerText =--countDown;
+                                setTimeout(()=>{
+                                    document.querySelector(".countdown").innerText =--countDown;
+                                    setTimeout(()=>{
+                                        document.querySelector(".countdown").innerText ="Happy Independence Day";
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
